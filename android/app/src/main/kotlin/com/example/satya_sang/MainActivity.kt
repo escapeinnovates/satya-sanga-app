df041847084833cdff95d57355ecc02286f16587
mayank-2777 <mssunthwal@gmail.com>
2026-01-10 16:22:28 +0530

    Clean main branch

--- android/app/src/main/kotlin/com/example/satya_sang/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.satya_sang
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- satya_sang/android/app/src/main/kotlin/com/example/satya_sang/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.satya_sang
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
